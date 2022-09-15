db0cd0fc187bc4f08dd07010990d1f4e07c6314b
aleksander meleshko <meleshkoad36@gmail.com>
2022-09-15 21:25:09 +0300

    Add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите целое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще одно целое число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+if(firstNum == secondNum * secondNum)
+{
+    Console.WriteLine($"{firstNum}, является квадратом числа {secondNum}");
+}
+else
+{
+    Console.WriteLine($"{firstNum}, не является квадратом числа {secondNum}");
+}
