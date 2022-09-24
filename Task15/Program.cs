733c56f358372357230f11db3a3b81b332e6fdd2
aleksander meleshko <meleshkoad36@gmail.com>
2022-09-24 17:53:03 +0300

    Add Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,18 +4,37 @@
 // 1 -> нет
 
 Console.WriteLine("Введите день недели: ");
-int num = Convert.ToInt32(Console.ReadLine());
 string numDay = Console.ReadLine();
 
-if (numDay == "6" | numDay == "7")
+if (numDay == "6")
 {
-    Console.WriteLine("Этот день выходной: да");
-} 
-else if (numDay < 1 | numDay > 7)
+    Console.WriteLine("Этот день выходной: да, это суббота");
+}
+else if (numDay == "7")
+{
+    Console.WriteLine("Этот день выходной: да, это воскресение");
+}
+else if (numDay == "1")
 {
     Console.WriteLine("Этот день выходной: нет");
 }
-else 
+else if (numDay == "2")
 {
-  Console.WriteLine("Это число не день недели");
+    Console.WriteLine("Этот день выходной: нет");
+}
+else if (numDay == "3")
+{
+    Console.WriteLine("Этот день выходной: нет");
+}
+else if (numDay == "4")
+{
+    Console.WriteLine("Этот день выходной: нет");
+}
+else if (numDay == "5")
+{
+    Console.WriteLine("Этот день выходной: нет");
+}
+else
+{
+    Console.WriteLine("Это число не день недели");
 }
