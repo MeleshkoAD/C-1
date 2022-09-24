fd2340980b454df963431e48699663ca4d3795b8
aleksander meleshko <meleshkoad36@gmail.com>
2022-09-24 14:22:53 +0300

    Add Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -4,37 +4,16 @@
 // 25, 5 -> да
 // 8, 9 -> нет
 
-void checkOnSquare(int arg1, int arg2)
-{
-    int sqr = 0;
-    if (arg1 < arg2)
-    {
-        sqr = arg1 * arg1;
-        if (sqr == arg2)
-        {
-            Console.WriteLine($"Число {arg2} является квадратом числа {arg1}");
-        }
-        else
-        {
-            Console.WriteLine($"Число {arg2} не является квадратом числа {arg1}");
-        }
-    }
-    else
-    {
-        sqr = arg2 * arg2;
-        if (sqr == arg1)
-        {
-            Console.WriteLine($"Число {arg1} является квадратом числа {arg2}");
-        }
-        else
-        {
-            Console.WriteLine($"Число {arg1} не является квадратом числа {arg2}");
-        }
-    }
-}
 
 Console.Write("Введите первое число: ");
-int firstNum = Convert.ToInt32(Console.ReadLine());
+int numA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int secondNum = Convert.ToInt32(Console.ReadLine());
-checkOnSquare(firstNum, secondNum);
+int numB = Convert.ToInt32(Console.ReadLine());
+
+bool IsPairSqure(int num1, int num2)
+{
+    return (num1 == num2 *num2) || (num2 == num1 * num1);
+}
+
+if (IsPairSqure(numA, numB)) Console.WriteLine($"{numA}, {numB} -> да");
+else Console.WriteLine($"{numA}, {numB} -> нет");
