31ea9f2537d2e032edac369ffcb5e9437324cf36
aleksander meleshko <meleshkoad36@gmail.com>
2022-10-19 15:44:02 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int DiapazNaturalNumbers = 0;
+for (int i = numberM; i <= numberN; i++)
+{
+    DiapazNaturalNumbers +=i; 
+}
+Console.WriteLine($"{DiapazNaturalNumbers} ");
